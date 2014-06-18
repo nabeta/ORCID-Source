df1a551cfbb71570ed8b0825f9d7cf7db4238702
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2014-06-18 14:56:40 -0600

    Test # 2

--- orcid-web/src/test/java/org/ci/test/FailTest.java
+++ orcid-web/src/test/java/org/ci/test/FailTest.java
@@ -1,3 +1,19 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2013 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
 package org.ci.test;
 
 import static org.junit.Assert.fail;
@@ -6,9 +22,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:test-orcid-frontend-web-servlet.xml" })
+@ContextConfiguration(locations = { "classpath:orcid-core-context.xml", "classpath:orcid-frontend-web-servlet.xml" })
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 public class FailTest {
 
     @Test
