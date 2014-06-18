52c8e82e0c6b0c7c8ac6195bd473a8f14e9e1385
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2014-06-18 15:29:38 -0600

    Should pass

--- orcid-web/src/test/java/org/ci/test/FailTest.java
+++ orcid-web/src/test/java/org/ci/test/FailTest.java
@@ -16,8 +16,6 @@
  */
 package org.ci.test;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
